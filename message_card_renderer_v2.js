f7cf51c3bebfb7537eead97715e6a84ac45974c3
mahgarg <mahgarg@microsoft.com>
2018-02-20 15:58:52 +0530

    hide/show function update

--- message_card_renderer_v2.js
+++ message_card_renderer_v2.js
@@ -727,6 +727,10 @@ function hideShowOriginalMessage() {
         showHideMessageDiv.innerHTML = "Show original message";
         bodyConatinerDiv.style.display = "none";
     }
+
+    var body = document.body;
+    var html = document.documentElement;
+
     var height = Math.max(body.scrollHeight, body.offsetHeight, html.clientHeight, html.scrollHeight, html.offsetHeight);
     onHeightChange(height);
 }
