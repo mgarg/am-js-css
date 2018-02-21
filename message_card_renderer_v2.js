78f3e2933818a6bf234228966554152604ca9596
mahgarg <mahgarg@microsoft.com>
2018-02-21 10:31:22 +0530

    overriding body padding

--- message_card_renderer_v2.js
+++ message_card_renderer_v2.js
@@ -228,7 +228,6 @@ MessageCardRenderer.prototype.renderCardJson = function(cardJson){
 
     var body = document.body;
     var html = document.documentElement;
-    body.style.padding = "8px";
     var height = Math.max(body.scrollHeight, body.offsetHeight, html.clientHeight, html.scrollHeight, html.offsetHeight);
     onHeightChange(height);
 }
@@ -249,6 +248,8 @@ MessageCardRenderer.prototype.render = function () {
 
         var body = document.body;
         var html = document.documentElement;
+
+        body.style.padding = "8px";
 
         var height = Math.max(body.scrollHeight, body.offsetHeight, html.clientHeight, html.scrollHeight, html.offsetHeight);
         onHeightChange(height);
