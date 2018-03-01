9296cbfd24ad765bb78b102b28e20a7cf8c08c66
mahgarg <mhkgarg@gmail.com>
2018-03-01 10:49:23 +0530

    calling onHeightChange in show/hide scenario

--- message_card_renderer_v2.js
+++ message_card_renderer_v2.js
@@ -875,7 +875,7 @@ function hideShowOriginalMessage() {
     var html = document.documentElement;
 
     var height = Math.max(body.scrollHeight, body.offsetHeight, html.clientHeight, html.scrollHeight, html.offsetHeight);
-    //onHeightChange(height);
+    onHeightChange(height);
 }
 
 var defaultCardConfig = {
